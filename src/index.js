bce7771c52fde8faad243b1e8fb7411583a7c088
Mateus Andrade <mateusandrade3@gmail.com>
2021-04-17 21:30:31 -0300

    Fixed typos

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public' + '/index.html'));
+  res.sendFile(path.join(__dirname, '../public' + '/index.html'));
 });
 
 app.get('/email-template', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public' + '/email-template.html'));
+  res.sendFile(path.join(__dirname, '../public' + '/email-template.html'));
 });
 
 app.listen(port, () => {
